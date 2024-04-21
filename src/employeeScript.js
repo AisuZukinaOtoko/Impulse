48b33b12ebc58871cdf25f8a47ab18e71197db33
Here06 <2543080@students.wits.ac.za>
2024-04-21 13:59:44 +0200

    Revamped Employee and Manager Homepages

--- src/employeeScript.js
+++ src/employeeScript.js
@@ -1,8 +1,29 @@
-function showSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.style.display='flex';
-    content.style.marginLeft="150px";
+
+function showsettingTile() {
+    var navSettings = document.getElementById("settingTile");
+    if (navSettings.style.display == "none") {
+        navSettings.style.display = "flex";
+    } else {
+        navSettings.style.display = "none";
+    }
+
+    var navAccount = document.getElementById("accountTile");
+    navAccount.style.display = "none";
 }
+
+function showaccountTile() {
+    var navAccount = document.getElementById("accountTile");
+    if (navAccount.style.display == "none") {
+        navAccount.style.display = "flex";
+    } else {
+        navAccount.style.display = "none";
+    }
+    var navSettings = document.getElementById("settingTile");
+    navSettings.style.display = "none";
+}
+
+
+
 function hideSidebar(){
     var sidebar =document.getElementById("sidebar");
     var content =document.getElementById("content");
