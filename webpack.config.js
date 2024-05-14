8d4cd05d6a1244bc15809512d7faa1055c3f54b2
AisuZukinaOtoko <kgolaganoma@gmail.com>
2024-05-14 17:18:09 +0200

    Added webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production', // development or production
   entry: './app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-//   resolve: {
-//     fallback: {
-//       "stream": false,
-//       "path": false,
-//       "util": false,
-//       "http": false,
-//       "assert": false,
-//       "crypto": false,
-//       "querystring": false,
-//       "os": false,
-//       "zlib": false,
-//       "https": false
-//     }
-//   }
+  target: 'node',
 };
 
