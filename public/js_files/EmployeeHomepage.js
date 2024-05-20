51f9d31cf122408f6348a46d08e7f36facdfcc9c
AisuZukinaOtoko <kgolaganoma@gmail.com>
2024-05-20 13:23:55 +0200

    Linking pages

--- public/js_files/EmployeeHomepage.js
+++ public/js_files/EmployeeHomepage.js
@@ -2,3 +2,7 @@
 document.getElementById('book_car_wash').addEventListener('click', function() {
     window.location.href = 'bookcarwash.html'; // Change the URL to the desired destination
 });
+
+function MoveToFeedback() {
+    window.location.href = 'http://localhost:3000/homepage/FeedbackStaff.html';
+  }
