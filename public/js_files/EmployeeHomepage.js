7b541b86e961ff1557d107ed2f9f3c856d190c62
UniqLeo <135037762+UniqLeo@users.noreply.github.com>
2024-05-20 12:38:54 +0200

    Notification cards hidden

--- public/js_files/EmployeeHomepage.js
+++ public/js_files/EmployeeHomepage.js
@@ -1,4 +1,4 @@
-
-document.getElementById('book_car_wash').addEventListener('click', function() {
-    window.location.href = 'bookcarwash.html'; // Change the URL to the desired destination
-});
+function storeData() {
+            localStorage.setItem('email', document.getElementById('Email').value);
+            
+}
